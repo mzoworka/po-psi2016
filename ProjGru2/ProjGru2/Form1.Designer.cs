25672ffdb1c91ec0af5c978adfab79627675a312
BeataGiza <beatagiza@outlook.com>
2016-03-08 18:05:12 +0100

    z miejscem na tekst odiberany, wysyłany i log

--- ProjGru2/ProjGru2/Form1.Designer.cs
+++ ProjGru2/ProjGru2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProjGru2
 {
-    partial class Form1
+    partial class frmSerwer
     {
         /// <summary>
         /// Wymagana zmienna projektanta.
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtOdbieranie = new System.Windows.Forms.RichTextBox();
+            this.txtWysylanie = new System.Windows.Forms.RichTextBox();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtOdbieranie
+            // 
+            this.txtOdbieranie.Location = new System.Drawing.Point(45, 27);
+            this.txtOdbieranie.Name = "txtOdbieranie";
+            this.txtOdbieranie.ReadOnly = true;
+            this.txtOdbieranie.Size = new System.Drawing.Size(349, 194);
+            this.txtOdbieranie.TabIndex = 0;
+            this.txtOdbieranie.Text = "";
+            // 
+            // txtWysylanie
+            // 
+            this.txtWysylanie.Location = new System.Drawing.Point(45, 256);
+            this.txtWysylanie.Name = "txtWysylanie";
+            this.txtWysylanie.Size = new System.Drawing.Size(349, 99);
+            this.txtWysylanie.TabIndex = 1;
+            this.txtWysylanie.Text = "";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(45, 392);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.Size = new System.Drawing.Size(349, 34);
+            this.txtLog.TabIndex = 2;
+            this.txtLog.Text = "";
+            // 
+            // frmSerwer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(505, 477);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtWysylanie);
+            this.Controls.Add(this.txtOdbieranie);
+            this.MaximizeBox = false;
+            this.Name = "frmSerwer";
+            this.Text = "Serwer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox txtOdbieranie;
+        private System.Windows.Forms.RichTextBox txtWysylanie;
+        private System.Windows.Forms.RichTextBox txtLog;
     }
 }
 
--- ProjGru2/ProjGru2/Form1.cs
+++ ProjGru2/ProjGru2/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace ProjGru2
 {
-    public partial class Form1 : Form
+    public partial class frmSerwer : Form
     {
-        public Form1()
+        public frmSerwer()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
